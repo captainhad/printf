451aade5dd67948346d713e79dd084380d22e1ca
captainhad <dawood.adam48@gmail.com>
2022-11-24 18:33:53 -0800

    A function that prints output according to a format

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - printf function
+ * @format: format input
+ * Return: return characters
+ */
+
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buffer_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flag = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer, flags,
+				width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;		
+		}
+	}
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+	return (precision_chars);
+}
